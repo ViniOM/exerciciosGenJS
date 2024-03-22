af9d9d2299206faff9a8ea7e4976efc38acf8b58
Vinicios <vini.webdeveloper@gmail.com>
2024-03-22 10:44:21 -0300

    fix if

--- lacos-condicionais/ex02.js
+++ lacos-condicionais/ex02.js
@@ -1,16 +1,16 @@
 const read = require("readline-sync");
 
-let b;
+let a, b;
 a = read.questionInt("Digite um numero ");
 
 if (a > 0) {
-  b = `positivo`;
+  b = `positivo!`;
 } else {
-  b = `negativo`;
+  b = `negativo!`;
 }
 
-if (a % 2 == 1) {
-  console.log(`${a} é um numero impar e ${b}`);
+if (a % 2 == 0) {
+  console.log(`${a} é um numero par e ${b}`);
 } else {
-  console.log(`${a} é um numero par e ${b}` );
+  console.log(`${a} é um numero impar e ${b}`);
 }
