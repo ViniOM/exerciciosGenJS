9dc7f94799856b79fe7bb314630c14a31048c2ed
Vinicios <vini.webdeveloper@gmail.com>
2024-03-22 11:39:59 -0300

    Contador de idade Resolvido

--- lacos-repeticao/for/ex02.js
+++ lacos-repeticao/for/ex02.js
@@ -1,1 +1,18 @@
-const read = require("")
+const read = require("readline-sync");
+
+let idade21 = 0,
+  idade50 = 0,
+  count = 0;
+
+while (count >= 0) {
+  count = read.questionInt("Qual sua idade");
+
+  if (count <= 21 && count > 0) {
+    idade21++;
+  } else if (count >= 50) {
+    idade50++;
+  }
+}
+
+console.log("Idade menores ou iguais a 21", idade21);
+console.log("\n Idade menores ou iguais a 50", idade50);
